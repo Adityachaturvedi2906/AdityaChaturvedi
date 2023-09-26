a5c1f12d88619c4acfb51cf0421304526d3eb8dd
Aditya chaturvedi <adityachaturvedi2906@gmail.com>
2023-09-26 13:19:26 +0530

    fourth Commit

--- components/section-divider.tsx
+++ components/section-divider.tsx
@@ -6,7 +6,7 @@ import { motion } from "framer-motion";
 export default function SectionDivider() {
   return (
     <motion.div
-      className="bg-gray-200 my-20 w-16 h-1 rounded-full  sm:block dark:bg-opacity-20"
+      className="bg-gray-200 !my-10 w-16 h-1 rounded-full  sm:block dark:bg-opacity-20"
       initial={{ opacity: 0, y: 100 }}
       animate={{ opacity: 1, y: 0 }}
       transition={{ delay: 0.125 }}
