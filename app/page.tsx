5488431b8a6ed206c4744605b4da4538476e2432
Resultant Edition <adityachaturvedi2906@gmail.com>
2026-09-17 01:23:33 +0530

    Prioritize POP teardown and Arcade; remove projects section

--- app/page.tsx
+++ app/page.tsx
@@ -2,8 +2,6 @@ import About from "@/components/about";
 import Contact from "@/components/contact";
 import Experience from "@/components/experience";
 import Intro from "@/components/intro";
-import Projects from "@/components/projects";
-import Skills from "@/components/skills";
 import Arcade from "@/components/arcade";
 import ProductNotes from "@/components/product-notes";
 
@@ -11,13 +9,11 @@ export default function Home() {
   return (
     <main className="flex flex-col items-center px-4">
       <Intro />
-      <Arcade />
       <ProductNotes />
+      <Arcade />
       <Experience />
-      <Skills />
-      <Projects />
       <About />
       <Contact />
     </main>
-  )
+  );
 }
